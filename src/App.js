fb405b6150e010a85dd8f04cfd026e16377416b0
徐小波 <xuxiaobo@372163.com>
2022-11-28 10:01:54 +0800

    初始化项目

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div className="App">
+        <Button type="primary">Primary Button</Button>
+        <Button>Default Button</Button>
+        <Button type="dashed">Dashed Button</Button>
+        <br />
+        <Button type="text">Text Button</Button>
+        <Button type="link">Link Button</Button>
+    </div>
+  );
+}
+
+export default App;
